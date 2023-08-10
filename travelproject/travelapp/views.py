c64cc8f1756df4c3462f77cfd0e600f58f99c91b
muhammedsinanms <sinanmuhammed994@gmail.com>
2023-08-10 14:40:08 +0530

    travel

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Place
+from . models import Team
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    pic=Team.objects.all()
+    return render(request,"index.html",{'result':obj,'staff':pic})
